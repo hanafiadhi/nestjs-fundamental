a483d1485f41b51769107881edad8853dcc8f2b8
hanafi <hanafiadhi890@gmail.com>
2023-08-26 16:31:21 +0700

    Module Service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -12,37 +12,43 @@ import {
   Res,
 } from '@nestjs/common';
 import { response } from 'express';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
   @Get()
   findAll(@Query() paginationQuery) {
     const { limit, offset } = paginationQuery;
-    return `this action returns all coffees Limit:${limit} and Query:${offset}`;
+    // return `this action returns all coffees Limit:${limit} and Query:${offset}`;
+    return this.coffeesService.findAll();
   }
   //   first
   //   findAll(@Res() response) {
   //     response.status(200).send('this action find all');
-  //   }
+  //   }z
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `this action return${id}`;
+    // return `this action return${id}`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.GONE) //http status menjadi 410 gone
-  create(@Body() Body) {
-    return Body;
+  //   @HttpCode(HttpStatus.GONE) //http status menjadi 410 gone
+  create(@Body() body) {
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() Body) {
-    return `this action updates #${id} coffee`;
+  update(@Param('id') id: string, @Body() body) {
+    // return `this action updates #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `this action removes #${id} coffee`;
+    // return `this action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Coffe } from './entities/coffees.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffe[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeDto: any) {
+    const existingCoffe = this.findOne(id);
+
+    if (existingCoffe) {
+      //do somthing
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
